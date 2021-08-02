d9975671a10203f2cf29b3a033d7281bba0d6d24
Ritik Bhardwaj <ritik.bhardwaj889@gmail.com>
2021-08-02 18:46:14 +0530

    setup the POST /images route - still not done with errors in the GET /images/:id route

--- index.js
+++ index.js
@@ -1,48 +1,68 @@
 require('dotenv').config();
-const app  = require('express')();
-const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
 const morgan = require('morgan');
 const helmet = require('helmet');
+const uniqid = require('uniqid');
+const multer = require('multer');
+const stream = require('stream');
 const fs = require('fs');
+const util = require('util');
+const unlinkFile = util.promisify(fs.unlink); //to promisify the fs.unlink function
 const path = require('path');
 const { uploadFile, getFileStream } = require('./s3');
-const PORT  = 3000 || process.env.PORT;
+const PORT = 3000 || process.env.PORT;
+
+//multer middleware setup
+const upload = multer({ dest: 'uploads/' });
 
 //middlewares
-app.use(morgan('common', {
-	stream: fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
-}));
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-
+app.use(
+	morgan('common', {
+		stream: fs.createWriteStream(path.join(__dirname, 'access.log'), {
+			flags: 'a',
+		}),
+	})
+);
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 //routes
 
 //get the image links from sql database
-app.get('/images', (req,res)=>{
+app.get('/images', (req, res) => {
 	const { base, limit } = req.query;
-	if(!isNaN(parseInt(limit)) && !isNaN(parseInt(limit))){
+	if (!isNaN(parseInt(limit)) && !isNaN(parseInt(limit))) {
 		res.send(req.query.base + ' ' + req.query.limit);
-	}else{
+	} else {
 		res.send('Please mention base and limit');
 	}
 });
 
 //get the image stream
-app.get('/images/:key', (req,res)=>{
-	const key  = req.params.key;
-	const readStream = getFileStream(key);
-	readStream.pipe(res);
+app.get('/images/:key', async (req, res) => {
+	const key = req.params.key;
+	const readableStream = new stream.Readable();
+	const fileStream = getFileStream(key);
+	fileStream.pipe(res);
 });
 
 //post image to the server
-app.post('/images',(req,res)=>{});
+app.post('/images', upload.single('image'), async (req, res) => {
+	const file = req.file;
+
+	//filter
+	//resize
+	//compress
+
+	const result = await uploadFile(file);
+	await unlinkFile(file.path);
+	console.log(result);
+});
 
 //delete a image with the id
-app.delete('images/:id', (req,res)=>{});
+app.delete('images/:id', (req, res) => {});
 
-
-
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
 	console.log(`Image server listening on - ${PORT}`);
 });
--- s3.js
+++ s3.js
@@ -4,35 +4,32 @@ const S3 = require('aws-sdk/clients/s3');
 
 const bucketName = process.env.AWS_BUCKET_NAME;
 const region = process.env.AWS_BUCKET_REGION;
-const accessKeyId  = process.env.AWS_ACCESS_KEY;
+const accessKeyId = process.env.AWS_ACCESS_KEY;
 const secretAccessKey = process.env.AWS_SECRET_KEY;
 
 const s3 = new S3({
 	region,
 	accessKeyId,
-	secretAccessKey
+	secretAccessKey,
 });
 
-
 //upload a file to s3
-exports.uploadFile = (file)=>{
+exports.uploadFile = (file) => {
 	const fileStream = fs.createReadStream(file.path);
-
 	const uploadParams = {
-		Bucket : bucketName,
+		Bucket: bucketName,
 		Body: fileStream,
-		Key: file.file.filename
-	}
-
+		Key: file.filename,
+	};
 	return s3.upload(uploadParams).promise();
-}
+	//s3.upload(uploadParams,(err,data)=>{cb(err,data)});
+};
 
 //download a file from s3
-exports.getFileStream = (fileKey)=>{
+exports.getFileStream = (fileKey) => {
 	const downloadParams = {
 		Key: fileKey,
-		Bucket: bucketName
-	}
-
+		Bucket: bucketName,
+	};
 	return s3.getObject(downloadParams).createReadStream();
-}
+};
